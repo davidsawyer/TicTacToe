8a2ef19693834646442fc21c5f1d5aa99fc0db1d
David Sawyer <davidsawyer10@gmail.com>
2012-09-10 00:42:31 -0400

    attempted removal

--- src/Board.java
+++
@@ -1,9 +0,0 @@
-// Board interface
-public interface Board {
-	
-	// Displays the board with any current modifcations
-    public void displayBoard();
-    
-    // takes in an int from the player representing a space on the board
-	public void turn(int space); 
-}
